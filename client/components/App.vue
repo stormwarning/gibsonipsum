3645c6e6f098257368a6ac5ac67f77da3df09230
Jeff <rustyangel@gmail.com>
2017-06-23 23:14:30 -0600

    🎨 Add custom body style

--- client/components/App.vue
+++ client/components/App.vue
@@ -3,3 +3,15 @@
     <router-view></router-view>
   </div>
 </template>
+
+<style>
+:root {
+    --bg: #16191b;
+    --fg: #e2e6e8;
+}
+
+body {
+    color: var(--fg);
+    background-color: var(--bg);
+}
+</style>
