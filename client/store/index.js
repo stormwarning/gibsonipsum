f6bfb0b567af570e17bdf247ecf384025b351326
Jeff <rustyangel@gmail.com>
2017-06-24 14:29:39 -0600

    ✨ Update fetch function to loop through pages

--- client/store/index.js
+++ client/store/index.js
@@ -2,6 +2,29 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 
+let wordlist = []
+let skip = 0
+
+function getAllWords (endpoint, callback) {
+  const getWords = fetch(endpoint)
+
+  getWords
+    .then(data => data.json())
+    .then((data) => {
+      for (const i in data.items) {
+        wordlist.push(data.items[i])
+      }
+
+      // Pages are always 100 items or less.
+      if (data.items.length < 100) {
+        callback(wordlist)
+      } else {
+        skip = skip + 100
+        getAllWords(endpoint + '&skip=' + skip, callback)
+      }
+    })
+}
+
 Vue.use(Vuex)
 
 const state = {
@@ -32,45 +55,17 @@ const actions = {
   fetchWords ({ state, commit }) {
     let dataURL = 'https://cdn.contentful.com/spaces/8j8wvx07a2uv/entries?access_token=f582803bba0fe0513deecb0f9edf8e0e0d31c631247ccc64d7d99087e7a75e85'
 
-    const getWords = fetch(dataURL)
+    getAllWords(dataURL, (wordlist) => {
+      commit('setWordList', wordlist)
 
-    getWords
-      .then(data => data.json())
-      .then((data) => {
-        let totalWords = data.total
-        let skip = 0
-        let wordlist = state.words
-        let collectedWords = wordlist.length > 0 ? wordlist.length : 0
-
-        console.log('totalWords:', totalWords)
-        console.log('items:', data.items.length)
-
-        for (const i in data.items) {
-          wordlist.push(data.items[i])
-        }
-
-        while (collectedWords < totalWords) {
-          collectedWords = collectedWords + wordlist.length
-          console.log('collectedWords:', collectedWords)
-          skip = skip + collectedWords
-          console.log('skip:', skip)
-
-          // @TODO: Repeatable callback.
-          // if (total > max) {
-          //   data = this.getWords(dataURL + '&skip=' + skip)
-          // }
-        }
-
-        commit('setWordList', wordlist)
-
-        let filteredWords = wordlist.filter((word) => {
-          return word.fields.type === 'adjective'
-        })
-        let w = Math.floor(Math.random() * filteredWords.length)
-        let adjective = filteredWords[w].fields.word
-
-        commit('setAdjective', adjective)
+      let filteredWords = wordlist.filter((word) => {
+        return word.fields.type === 'adjective'
       })
+      let w = Math.floor(Math.random() * filteredWords.length)
+      let adjective = filteredWords[w].fields.word
+
+      commit('setAdjective', adjective)
+    })
   },
   findAdjective ({ state, commit }) {
     let words = state.words
