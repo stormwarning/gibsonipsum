fd93116f783a714e7900f7342a1f95c465179081
Jeff <rustyangel@gmail.com>
2017-06-24 14:29:10 -0600

    ⚙️ Change build dir name & add now.sh config

--- build/utils.js
+++ build/utils.js
@@ -23,11 +23,11 @@ _.cssProcessors = [
 
 _.outputPath = config.electron ?
   path.join(__dirname, '../app/dist') :
-  path.join(__dirname, '../gibsonipsum')
+  path.join(__dirname, '../dist')
 
 _.outputIndexPath = config.electron ?
   path.join(__dirname, '../app/dist/index.html') :
-  path.join(__dirname, '../gibsonipsum/index.html')
+  path.join(__dirname, '../dist/index.html')
 
 _.target = config.electron ?
   'electron-renderer' :
