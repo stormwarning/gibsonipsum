33d85f0e2e5cfb259e0f22f4773347fd0b6ef9c6
Jeff <rustyangel@gmail.com>
2017-06-25 13:58:49 -0600

    ✏️ Fix page title
    Lorem Gibson already exists...

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="cf ph3 ph5-ns pv5">
         <header class="fn fl-ns w-50-ns pr4-ns">
-            <h1 class="mb3 mt0 lh-title">Lorem Gibson</h1>
+            <h1 class="mb3 mt0 lh-title">Gibson Ipsum</h1>
             <intro></intro>
         </header>
         <ipsum></ipsum>
