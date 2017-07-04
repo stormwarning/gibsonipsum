a7b894ad7e7a59eca9b18fb240c12ab00053fd2e
Jeff <rustyangel@gmail.com>
2017-07-03 21:07:33 -0600

    ✨ Add Controls component

--- client/store/index.js
+++ client/store/index.js
@@ -6,74 +6,81 @@ let wordlist = []
 let skip = 0
 
 function getAllWords (endpoint, callback) {
-  const getWords = fetch(endpoint)
+    const getWords = fetch(endpoint)
 
-  getWords
-    .then(data => data.json())
-    .then((data) => {
-      for (const i in data.items) {
-        wordlist.push(data.items[i])
-      }
+    getWords
+        .then(data => data.json())
+        .then((data) => {
+            for (const i in data.items) {
+                wordlist.push(data.items[i])
+            }
 
-      // Pages are always 100 items or less.
-      if (data.items.length < 100) {
-        callback(wordlist)
-      } else {
-        skip = skip + 100
-        getAllWords(endpoint + '&skip=' + skip, callback)
-      }
-    })
+            // Pages are always 100 items or less.
+            if (data.items.length < 100) {
+                callback(wordlist)
+            } else {
+                skip = skip + 100
+                getAllWords(endpoint + '&skip=' + skip, callback)
+            }
+        })
 }
 
 Vue.use(Vuex)
 
 const state = {
-  paragraphs: 3,
-  sentences: 5,
-  l33t: false,
-  nihongo: false,
-  words: [],
-  randomAdjective: 'cyberpunk',
+    paragraphs: 3,
+    sentences: 5,
+    l33t: false,
+    nihongo: false,
+    words: [],
+    randomAdjective: 'cyberpunk',
 }
 
 const getters = {
-  getParagraphs: state => state.paragraphs,
-  getWordList: state => state.words,
-  getAdjective: state => state.randomAdjective,
+    getParagraphs: state => state.paragraphs,
+    getSentences: state => state.sentences,
+    getWordList: state => state.words,
+    getAdjective: state => state.randomAdjective,
 }
 
 const mutations = {
-  setWordList (state, wordData) {
-    state.words = wordData
-  },
-  setAdjective (state, adjective) {
-    state.randomAdjective = adjective
-  },
+    setParagraphs (state, number) {
+        state.paragraphs = number
+    },
+    setSentences (state, number) {
+        state.sentences = number
+    },
+    setWordList (state, wordData) {
+        state.words = wordData
+    },
+    setAdjective (state, adjective) {
+        state.randomAdjective = adjective
+    },
 }
 
 const actions = {
-  fetchWords ({ state, commit }) {
-    let dataURL = 'https://cdn.contentful.com/spaces/8j8wvx07a2uv/entries?access_token=f582803bba0fe0513deecb0f9edf8e0e0d31c631247ccc64d7d99087e7a75e85'
+    fetchWords ({ state, commit }) {
+        let dataURL = 'https://cdn.contentful.com/spaces/8j8wvx07a2uv/entries?access_token=f582803bba0fe0513deecb0f9edf8e0e0d31c631247ccc64d7d99087e7a75e85'
 
-    getAllWords(dataURL, (wordlist) => {
-      commit('setWordList', wordlist)
+        getAllWords(dataURL, (wordlist) => {
+            commit('setWordList', wordlist)
 
-      let filteredWords = wordlist.filter((word) => {
-        return word.fields.type === 'adjective' && word.fields.tags.includes('compound')
-      })
-      let w = Math.floor(Math.random() * filteredWords.length)
-      let adjective = filteredWords[w].fields.word
+            let filteredWords = wordlist.filter((word) => {
+                return word.fields.type === 'adjective' && word.fields.tags.includes('compound')
+            })
+            let w = Math.floor(Math.random() * filteredWords.length)
+            let adjective = filteredWords[w].fields.word
 
-      commit('setAdjective', adjective)
-    })
-  },
+            commit('setAdjective', adjective)
+        })
+    },
 }
 
 const store = new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions
+    state,
+    getters,
+    mutations,
+    actions
 })
 
 export default store
--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-    <article class="">
-        <header class="relative mv5">
-            <figure class="absolute absolute--fill z-0 ma0 cover o-50" style="background-image:url(/gibson-glitch.png)"></figure>
-            <div class="relative">
-                <h1 class="mb3 mt7 white lh-title f-headline">Gibson Ipsum</h1>
+    <article class="relative vh-100">
+        <header class="fixed flex flex-column justify-between w-50 vh-100">
+            <figure class="absolute absolute--fill z-0 ma0 cover o-50 mh6 mv5" style="background-image:url(/gibson-glitch.png)"></figure>
+            <div class="relative pl5">
+                <h1 class="mb3 mt6 white lh-solid f-headline tr">Gibson Ipsum</h1>
                 <intro></intro>
                 <!--<span class="dib mr4 mr5-ns">By @stormwarning</span>-->
             </div>
+            <controls></controls>
         </header>
-        <aside class="controls">—</aside>
         <ipsum></ipsum>
     </article>
 </template>
@@ -16,11 +16,13 @@
 <script>
     import Ipsum from 'components/Ipsum'
     import Intro from 'components/Intro'
+    import Controls from 'components/Controls'
 
     export default {
         components: {
             Ipsum,
             Intro,
+            Controls,
         },
     }
 </script>
