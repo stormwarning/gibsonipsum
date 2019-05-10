79b98665df2bf6ab15f50375efc0bfa754a83a80
Jeff <rustyangel@gmail.com>
2019-05-10 09:54:04 -0600

    Update wordText formatting logic

--- components/Word.vue
+++ components/Word.vue
@@ -1,8 +1,10 @@
 <template>
-    <span>{{ wordText }}</span>
+    <span v-html="wordText"></span>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     props: {
         word: {
@@ -22,33 +24,45 @@ export default {
     },
 
     data() {
-        return {
-            shouldTranslate: this.$store.state.nihongo,
-        }
+        return {}
     },
 
     computed: {
+        ...mapGetters({
+            shouldTranslate: 'getNihongo',
+        }),
+
         wordText() {
             let text = this.word.fields.word
 
             if (this.startsSentence) {
-                text = text.charAt(0).toUpperCase() + text.slice(1) + ' '
-            } else if (this.endsSentence) {
-                text = text + '. '
-            } else {
-                if (
-                    this.shouldTranslate &&
-                    this.word.fields.tags &&
-                    this.word.fields.tags.includes('foreign')
-                ) {
-                    text = `<i class="nowrap fs-normal" lang="jp" title="${
-                        this.word.fields.word
-                    }">${this.word.fields.def}</i>`
-                }
-                text = text + ' '
+                text = text.charAt(0).toUpperCase() + text.slice(1)
             }
 
-            return text
+            if (
+                this.word.fields.tags &&
+                this.word.fields.tags.includes('acronym')
+            ) {
+                text = `<abbr class="c2sc" title="${this.word.fields.def}">${
+                    this.word.fields.word
+                }</abbr>`
+            }
+
+            if (
+                this.shouldTranslate &&
+                this.word.fields.tags &&
+                this.word.fields.tags.includes('foreign')
+            ) {
+                text = `<i class="nowrap fs-normal" lang="jp" title="${
+                    this.word.fields.word
+                }">${this.word.fields.def}</i>`
+            }
+
+            if (this.endsSentence) {
+                text = `${text}.`
+            }
+
+            return `${text} `
         },
     },
 }
