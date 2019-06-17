cc97add6759640379af09e3f70d622337534558c
Jeff <rustyangel@gmail.com>
2019-06-16 22:56:42 -0600

    Update spacing above Intro text

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,12 +1,12 @@
 <template>
-    <span class="f6 lh-copy ttu tracked white-50">
+    <div class="mt4 mt3-ns f6 lh-copy ttu tracked white-50">
         A {{ adjective }} placeholder text generator.
         <br />
         By
         <a class="link white-70 hover-gibson" href="http://tidaltheory.co/">
             @stormwarning
         </a>
-    </span>
+    </div>
 </template>
 
 <script>
