0b03ff91f5542fbe48ec9e4251f458170e8575cc
Jeff <rustyangel@gmail.com>
2019-08-09 21:57:57 -0600

    Update Controls drawer transition

--- components/TransitionHeight.vue
+++ components/TransitionHeight.vue
@@ -1,4 +1,7 @@
 <script>
+/**
+ * @see https://markus.oberlehner.net/blog/transition-to-height-auto-with-vue/
+ */
 export default {
     name: `TransitionExpand`,
     functional: true,
@@ -13,43 +16,49 @@ export default {
                 },
                 enter(element) {
                     let { width } = getComputedStyle(element)
-                    /* eslint-disable no-param-reassign */
+
                     element.style.width = width
                     element.style.position = `absolute`
                     element.style.visibility = `hidden`
                     element.style.height = `auto`
-                    /* eslint-enable */
+
                     let { height } = getComputedStyle(element)
-                    /* eslint-disable no-param-reassign */
+
                     element.style.width = null
                     element.style.position = null
                     element.style.visibility = null
                     element.style.height = 0
-                    /* eslint-enable */
-                    // Force repaint to make sure the
-                    // animation is triggered correctly.
+
+                    /**
+                     * Force a repaint to make sure theanimation is
+                     * triggered correctly.
+                     */
                     // eslint-disable-next-line no-unused-expressions
                     getComputedStyle(element).height
+
                     setTimeout(() => {
-                        // eslint-disable-next-line no-param-reassign
                         element.style.height = height
                     })
                 },
                 leave(element) {
                     let { height } = getComputedStyle(element)
-                    // eslint-disable-next-line no-param-reassign
+
                     element.style.height = height
-                    // Force repaint to make sure the
-                    // animation is triggered correctly.
+
+                    /**
+                     * Force a repaint to make sure theanimation is
+                     * triggered correctly.
+                     */
                     // eslint-disable-next-line no-unused-expressions
                     getComputedStyle(element).height
+
                     setTimeout(() => {
-                        // eslint-disable-next-line no-param-reassign
                         element.style.height = 0
                     })
                 },
             },
         }
+
         return createElement(`transition`, data, context.children)
     },
 }
@@ -68,10 +77,13 @@ export default {
 .expand-enter-active,
 .expand-leave-active {
     overflow: hidden;
-    transition: height 1s ease-in-out;
+    transition: 1000ms ease-in-out;
+    transition-property: height, opacity;
 }
+
 .expand-enter,
 .expand-leave-to {
     height: 0;
+    opacity: 0;
 }
 </style>
