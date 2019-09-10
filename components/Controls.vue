4861799c1d98dd38e610fdce7de588205c489367
Jeff <rustyangel@gmail.com>
2019-09-09 22:20:21 -0600

    🎨 Add custom checkbox control

--- components/Controls.vue
+++ components/Controls.vue
@@ -27,7 +27,14 @@
                 <div class="f6 fw5 tracked lh-title ttu o-80">
                     Japanese
                 </div>
-                <input v-model="nihongo" type="checkbox" />
+                <div class="relative mt2 checkbox">
+                    <input
+                        v-model="nihongo"
+                        class="absolute checkbox-input"
+                        type="checkbox"
+                    />
+                    <div class="dib na1 br1 checkbox-control" />
+                </div>
             </label>
         </form>
     </aside>
@@ -104,6 +111,50 @@ export default {
 .nihongo-field {
     @media (min-width: 60em) {
         margin-top: 2.5rem;
+    }
+}
+
+.checkbox {
+    position: relative;
+    top: -1px;
+}
+
+.checkbox-control {
+    &::before {
+        display: block;
+        width: 1rem;
+        height: 1rem;
+        margin: 0.25rem;
+        content: '';
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+}
+
+.checkbox-input {
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+
+    &:checked {
+        & ~ .checkbox-control::before {
+            background-color: #fff;
+        }
+    }
+
+    &:focus {
+        & ~ .checkbox-control {
+            box-shadow: 0 0 0 2px var(--blue);
+        }
+    }
+
+    &:active {
+        & ~ .checkbox-control::before {
+            background-color: rgba(255, 255, 255, 0.6);
+        }
     }
 }
 </style>
